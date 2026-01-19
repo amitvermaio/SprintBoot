5fdc6887181bcd13217f5659f6d72f198b4edef5
amitvermaio <amitvns159@gmail.com>
2026-01-19 15:16:56 +0530

    multiple data send using ArrayList<ModelType>

--- Day2_part2/src/main/java/com/example/day2_part2/controllers/HomeController.java
+++ Day2_part2/src/main/java/com/example/day2_part2/controllers/HomeController.java
@@ -4,12 +4,24 @@ import com.example.day2_part2.models.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class HomeController {
 
     @GetMapping("/student")
-    public StudentModel getStudent() {
-        StudentModel student = new StudentModel(1, "Amit", "amit@email.com");
-        return student;
+    public ArrayList<StudentModel> getStudent() {
+        ArrayList<StudentModel> students = new ArrayList<>();
+        StudentModel student1 = new StudentModel(1, "Amit", "amit@email.com");
+        StudentModel student2 = new StudentModel(2, "Ayush", "ayush@email.com");
+        StudentModel student3 = new StudentModel(3, "Rohan", "rohan@email.com");
+        StudentModel student4 = new StudentModel(4, "Ram", "ram@email.com");
+        StudentModel student5 = new StudentModel(5, "React", "react@email.com");
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+        students.add(student5);
+        return students;
     }
 }
