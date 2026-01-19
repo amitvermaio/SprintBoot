4fc43257ab465f8d500e37435d2ec5916d4cb5c0
amitvermaio <amitvns159@gmail.com>
2026-01-19 15:38:35 +0530

    api

--- Day2_part2/src/main/java/com/example/day2_part2/controllers/HomeController.java
+++ Day2_part2/src/main/java/com/example/day2_part2/controllers/HomeController.java
@@ -2,26 +2,36 @@ package com.example.day2_part2.controllers;
 
 import com.example.day2_part2.models.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-
 @RestController
 public class HomeController {
 
+    private final ArrayList<StudentModel> students;
+
+    public HomeController() {
+        students = new ArrayList<>();
+        students.add(new StudentModel(1, "Amit", "amit@email.com"));
+        students.add(new StudentModel(2, "Ayush", "ayush@email.com"));
+        students.add(new StudentModel(3, "Rohan", "rohan@email.com"));
+        students.add(new StudentModel(4, "Ram", "ram@email.com"));
+        students.add(new StudentModel(5, "React", "react@email.com"));
+    }
+
     @GetMapping("/student")
     public ArrayList<StudentModel> getStudent() {
-        ArrayList<StudentModel> students = new ArrayList<>();
-        StudentModel student1 = new StudentModel(1, "Amit", "amit@email.com");
-        StudentModel student2 = new StudentModel(2, "Ayush", "ayush@email.com");
-        StudentModel student3 = new StudentModel(3, "Rohan", "rohan@email.com");
-        StudentModel student4 = new StudentModel(4, "Ram", "ram@email.com");
-        StudentModel student5 = new StudentModel(5, "React", "react@email.com");
-        students.add(student1);
-        students.add(student2);
-        students.add(student3);
-        students.add(student4);
-        students.add(student5);
         return students;
+    }
+
+    @GetMapping("/student/{studentId}")
+    public StudentModel studentById(@PathVariable int studentId) {
+        for (StudentModel student : students) {
+            if (student.getId() == studentId) {
+                return student;
+            }
+        }
+        return null;
     }
 }
