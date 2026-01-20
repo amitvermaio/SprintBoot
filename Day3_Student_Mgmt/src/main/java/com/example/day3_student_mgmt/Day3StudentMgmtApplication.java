b6f340a8b92e686ee8363dd41f30fcf98fd295bd
amitvermaio <amitvns159@gmail.com>
2026-01-20 11:49:56 +0530

    Student Management System MongoDB

---
+++ Day3_Student_Mgmt/src/main/java/com/example/day3_student_mgmt/Day3StudentMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.example.day3_student_mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3StudentMgmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day3StudentMgmtApplication.class, args);
+    }
+
+}
---
+++ Day3_Student_Mgmt/src/test/java/com/example/day3_student_mgmt/Day3StudentMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day3_student_mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3StudentMgmtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
