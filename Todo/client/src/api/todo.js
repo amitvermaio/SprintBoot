e711700687b921460e3751309099e009684495c0
amitvermaio <amitvns159@gmail.com>
2026-02-16 16:08:31 +0530

    Todo Done

--- Todo/client/src/api/todo.js
+++
@@ -1,9 +0,0 @@
-import api from './axios.js';
-
-export const getTodosRequest = () => api.get('/todo/');
-
-export const createTodoRequest = (payload) => api.post('/todo/', payload);
-
-export const updateTodoRequest = (id, updates) => api.patch(`/todo/${id}`, updates);
-
-export const deleteTodoRequest = (id) => api.delete(`/todo/${id}`);
